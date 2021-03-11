166c6959858958e9a39ccd85f484a1a92fbc2c9f
= <=>
2021-03-11 17:51:05 +0000

    new css rules

---
+++ assets/config/tailwind.dev.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+	enabled: false,
+	content: ['./index.html']
+	},
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
